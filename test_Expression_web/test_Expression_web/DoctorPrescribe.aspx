5b013a56a569399ef96e0846cce596bf6dce58b9
RuvinT <jagodaruvin@gmail.com>
2018-01-21 08:49:28 +0530

    Password reset

--- test_Expression_web/test_Expression_web/DoctorPrescribe.aspx
+++ test_Expression_web/test_Expression_web/DoctorPrescribe.aspx
@@ -1,15 +1,47 @@
 ﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="DoctorPrescribe.aspx.cs" Inherits="test_Expression_web.DoctorPrescribe" %>
 
-<!DOCTYPE html>
 
-<html xmlns="http://www.w3.org/1999/xhtml">
+<!DOCTYPE html>
+<html >
 <head runat="server">
-    <title></title>
+  <meta charset="UTF-8">
+  <title>Doctor prescription</title>
+  <meta name="viewport" content="width=device-width, initial-scale=1"><link href='https://fonts.googleapis.com/css?family=Roboto:400,700' rel='stylesheet' type='text/css'>
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/normalize/5.0.0/normalize.min.css">
+
+  
+      <link rel="stylesheet" href="css/style11.css">
+
+  
 </head>
+
 <body>
-    <form id="form1" runat="server">
-        <div>
+     
+  <div class="user">
+    <header class="user__header">
+        <img src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/3219/logo.svg" alt="" />
+        <h1 class="user__title">Prescription</h1>
+    </header>
+    
+    <form class="form" runat="server">
+        <div class="form__group">
+            <asp:Label ID="Label1"  class="form__input" runat="server" Text="Label"></asp:Label>
         </div>
+        
+        <div class="form__group">
+            <asp:Label ID="Label2"  class="form__input" runat="server" Text="Label"></asp:Label>
+        </div>
+        
+        <div class="form__group">
+            <asp:Label ID="Label3"  class="form__input" runat="server" Text="Label"></asp:Label>
+        </div>
+        
+        <button class="btn" type="button">Register</button>
     </form>
+</div>
+  <script src='http://cdnjs.cloudflare.com/ajax/libs/jquery/2.1.3/jquery.min.js'></script>
+
+    <script src="js/index.js"></script>
+
 </body>
 </html>
---
+++ test_Expression_web/test_Expression_web/Doctorlogin.aspx.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Data.SqlClient;
+using System.Configuration;
+
+namespace test_Expression_web
+{
+    public partial class Doctorlogin : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+        protected void log_Click(object sender, EventArgs e)
+        {
+
+            SqlConnection conn = new SqlConnection(ConfigurationManager.ConnectionStrings["HealthPackConnectionString"].ConnectionString);
+            conn.Open();
+
+            String str = "select count(*) from Consultant where Email ='" + checkemail.Text + "'";
+
+            SqlCommand com = new SqlCommand(str, conn);
+            int temp = Convert.ToInt32(com.ExecuteScalar().ToString());
+
+            if (temp == 1)
+            {
+
+                string checkPasswordQuery = "select Password from Consultant where  Email ='" + checkemail.Text + "'";
+                SqlCommand passComm = new SqlCommand(checkPasswordQuery, conn);
+
+                string pass = passComm.ExecuteScalar().ToString();
+                if (pass == checkpassword.Text)
+                {
+
+                    Response.Write("Password is Correct");
+
+                    Response.Redirect("DoctorPrescribe.aspx");
+                    conn.Close();
+                }
+                else
+                {
+                    Response.Write(" password not match");
+                }
+
+
+
+
+            }
+            else
+            {
+                Response.Write("Email  not match");
+            }
+
+
+
+
+            conn.Close();
+
+        }
+    }
+}
