3bd8f967cec2da31295e2c9c6b6f0d614aff8346
RuvinT <jagodaruvin@gmail.com>
2018-03-13 11:33:06 +0530

    Add files via upload

---
+++ SQLQuery1.sql
@@ -0,0 +1,18 @@
+﻿create table PharmacyDrug(
+PharmacyId varchar (50),
+DrugBrand    varchar(50),
+DrugName	 varchar(50),
+DrugCount  varchar(50),
+Available	 varchar(50),
+primary key (DrugBrand)
+);
+
+select * from PharmacyDrug
+
+insert into PharmacyDrug values ('123','Altace','warfarin','120','Available');
+insert into PharmacyDrug values ('123','lorazepam','Ativan','150','Available');
+insert into PharmacyDrug values ('123','Diabeta','glyburide','200','Not available');
+insert into PharmacyDrug values ('123','Flonase','fluticasone','50','Not available');
+insert into PharmacyDrug values ('123','albuterol','Ventolin','20','Available');
+insert into PharmacyDrug values ('123','Zoloft','sertraline','90','Available');
+insert into PharmacyDrug values ('123','Glucotrol','glipizide','60','Available');
